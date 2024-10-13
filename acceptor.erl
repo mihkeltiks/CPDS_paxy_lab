1279f3451945bc4396e3a2fc48c8bb62fe238bd7
Patrick <patrickcerkalb@gmail.com>
2024-10-13 23:22:02 +0200

    crash working but not recovering

--- acceptor.erl
+++ acceptor.erl
@@ -1,8 +1,7 @@
 -module(acceptor).
--export([start/2]).
+-export([start/2, crash/1]).
 -define(drop_ratio, 0). % Drops 1 in 10 messages
 -define(delay, 1).
-
 
 send_maybe_drop(Pid, Message) ->
     P = rand:uniform(10),
@@ -12,60 +11,71 @@ send_maybe_drop(Pid, Message) ->
     true ->
         T = rand:uniform(?delay),
         timer:send_after(T, Pid, Message)
-        % Pid ! Message
     end.
 
-
 start(Name, PanelId) ->
-  spawn(fun() -> init(Name, PanelId) end).
-        
+    spawn(fun() -> init(Name, PanelId) end).
+
 init(Name, PanelId) ->
-  Promised = order:null(), 
-  Voted = order:null(),
-  Value = na,
-  acceptor(Name, Promised, Voted, Value, PanelId).
+    pers:open(Name),
+    {Promised, Voted, Value, _} = case pers:read(Name) of
+        {ok, State} -> State;
+        _ -> {order:null(), order:null(), na, na}
+    end,
+    acceptor(Name, Promised, Voted, Value, PanelId).
 
 acceptor(Name, Promised, Voted, Value, PanelId) ->
-  receive 
-    {prepare, Proposer, Round} ->
-      case order:gr(Round, Promised) of
-        true ->
-          % Proposer  ! {promise, Round, Voted, Value},
-          send_maybe_drop(Proposer, {promise, Round, Voted, Value}) ,             
-      io:format("[Acceptor ~w] Phase 1: promised ~w voted ~w colour ~w~n",
-                 [Name, Round, Voted, Value]),
-          % Update gui
-          Colour = case Value of na -> {0,0,0}; _ -> Value end,
-          PanelId ! {updateAcc, "Voted: " ++ io_lib:format("~p", [Voted]), 
-                     "Promised: " ++ io_lib:format("~p", [Round]), Colour},
-          acceptor(Name, Round, Voted, Value, PanelId);
-        false ->
-         % Proposer  ! {sorry, {prepare, Round}},
-         send_maybe_drop(Proposer, {sorry, {prepare, Round}}),
-          acceptor(Name, Promised, Voted, Value, PanelId)
-      end;
-    {accept, Proposer, Round, Proposal} ->
-      case order:goe(Round, Promised) of
-        true ->
-          % Proposer  ! {vote, Round},
-          send_maybe_drop(Proposer, {vote, Round}),
-          case order:goe(Round, Voted) of
-            true ->
-      io:format("[Acceptor ~w] Phase 2: promised ~w voted ~w colour ~w~n",
-                 [Name, Promised, Round, Proposal]),
-              % Update gui
-              PanelId ! {updateAcc, "Voted: " ++ io_lib:format("~p", [Round]), 
-                         "Promised: " ++ io_lib:format("~p", [Promised]), Proposal},
-              acceptor(Name, Promised, Round, Proposal, PanelId);
-            false ->
-              acceptor(Name, Promised, Voted, Value, PanelId)
-          end;                            
-        false ->
-          % Proposer  ! {sorry, {accept, Round}},
-          send_maybe_drop(Proposer, {sorry, {accept, Round}}),
-          acceptor(Name, Promised, Voted, Value, PanelId)
-      end;
-    stop ->
-      PanelId ! stop,
-      ok
-  end.
+    receive
+        {prepare, Proposer, Round} ->
+            case order:gr(Round, Promised) of
+                true ->
+                    send_maybe_drop(Proposer, {promise, Round, Voted, Value}),
+                    io:format("[Acceptor ~w] Phase 1: promised ~w voted ~w colour ~w~n",
+                              [Name, Round, Voted, Value]),
+                    Colour = case Value of na -> {0,0,0}; _ -> Value end,
+                    PanelId ! {updateAcc, "Voted: " ++ io_lib:format("~p", [Voted]),
+                               "Promised: " ++ io_lib:format("~p", [Round]), Colour},
+                    pers:store(Name, Round, Voted, Value, PanelId),
+                    acceptor(Name, Round, Voted, Value, PanelId);
+                false ->
+                    send_maybe_drop(Proposer, {sorry, {prepare, Round}}),
+                    acceptor(Name, Promised, Voted, Value, PanelId)
+            end;
+        {accept, Proposer, Round, Proposal} ->
+            case order:goe(Round, Promised) of
+                true ->
+                    send_maybe_drop(Proposer, {vote, Round}),
+                    case order:goe(Round, Voted) of
+                        true ->
+                            io:format("[Acceptor ~w] Phase 2: promised ~w voted ~w colour ~w~n",
+                                      [Name, Promised, Round, Proposal]),
+                            PanelId ! {updateAcc, "Voted: " ++ io_lib:format("~p", [Round]),
+                                       "Promised: " ++ io_lib:format("~p", [Promised]), Proposal},
+                            pers:store(Name, Promised, Round, Proposal, PanelId),
+                            acceptor(Name, Promised, Round, Proposal, PanelId);
+                        false ->
+                            acceptor(Name, Promised, Voted, Value, PanelId)
+                    end;
+                false ->
+                    send_maybe_drop(Proposer, {sorry, {accept, Round}}),
+                    acceptor(Name, Promised, Voted, Value, PanelId)
+            end;
+        stop ->
+            PanelId ! stop,
+            pers:delete(Name),
+            ok
+    end.
+
+crash(Name) ->
+    case whereis(Name) of
+        undefined -> ok;
+        Pid ->
+            unregister(Name),
+            exit(Pid, "crash"),
+            pers:open(Name),
+            {_, _, _, Pn} = pers:read(Name),
+            Pn ! {updateAcc, "Voted: CRASHED", "Promised: CRASHED", {0,0,0}},
+            pers:close(Name),
+            timer:sleep(1000),
+            register(Name, acceptor:start(Name, na))
+    end.
